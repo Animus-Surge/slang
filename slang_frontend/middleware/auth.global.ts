75c0a90db18f3fa05c78316e76d93e034836eb5f
Surge <esfloyd9341@gmail.com>
2023-08-17 20:42:42 -0400

    Frontend backup

--- slang_frontend/middleware/auth.global.ts
+++ slang_frontend/middleware/auth.global.ts
@@ -1,3 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const authCookie = useCookie('slang-auth')
+
+    //Let's first check the path we're going to
+    if (authCookie && to.path == "/login") {
+
+    }
 })
